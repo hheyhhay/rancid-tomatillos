244273f01f18ad5febd8b7509dc00d762b77f2a1
Eric Li <ericli@knights.ucf.edu>
2021-08-25 16:45:11 -0600

    Import CSS file, edit HTML elements

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Card.css';
 
 const Card = (props) => {
   console.log(props)
   return (
-    <div>
-      <img src={props.posterPath}/>
+    <div className='movie-card'>
+      <img className='movie-poster' height='200px' src={props.posterPath}/>
+      <div className='movie-text-front'>
+        <h3 className='movie-title'>{props.title}</h3>
+        <p className='movie-rating'>⭐️ {props.averageRating.toFixed(1)}</p>
+      </div>
     </div>
   )
 }
