e34e0f0e702e9d0a2a52fce49f191e913813efe9
Hayley Dixon <hdixon37@gmail.com>
2021-08-26 16:22:35 -0600

    Add all details to back of card

--- src/Details.js
+++ src/Details.js
@@ -5,14 +5,22 @@ const Details = (props) => {
   console.log(props)
   return (
     <div className='movie-details' onClick ={() => props.showDetails(props.id)} >
-      <img className='movie-poster' height='200px' src={props.posterPath}/>
+      <img className='movie-backdrop' height='200px' src={props.backdropPath}/>
       <h3 className='movie-title'>{props.title}</h3>
-      <p className='movie-genres'>{props.genres}</p>
-      <p className='movie-rating'>⭐️ {props.averageRating.toFixed(1)}</p>
       <p className='tagline'>{props.tagline}</p>
+      <p className='movie-rating'>⭐️ {props.averageRating.toFixed(1)}</p>
+      <p className="movie-overview">{props.overview}</p>
+      <p className='budget'>BUDGET: ${props.budget}</p>
+      <p className='revenue'>REVENUE: ${props.revenue}</p>
+      <p className='runtime'>RUNTIME: {props.runtime} mins </p>
+      <p className='movie-releaseDate'>Released on: {props.releaseDate}</p>
     </div>
   )
 }
 
 export default Details;
 // instead of props.showDetail we will have a props.goBack()
+// To be added to Details:
+// Take out: genres
+// leave-in: tagline, rating, title, overview, backdrop, releaseDate,
+//budget, revenue, runtime
