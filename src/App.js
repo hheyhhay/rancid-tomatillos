0264ff5dc9f359607400340260644af6d443e357
Eric Li <ericli@knights.ucf.edu>
2021-08-28 15:59:26 -0600

    Add error handling for fetch call

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
       this.state = {
         movies: [],
+        error: ''
       }
   }
 
@@ -26,12 +27,14 @@ class App extends Component {
     fetch(url)
       .then(res => res.json())
       .then(data => this.setState({movies: data.movies}))
+      .catch(error => this.setState({error: error}))
   }
 
   render() {
     return (
       <main className='App'>
         <h2>Movies</h2>
+        {this.state.error && <h3 className='error'>Movies to failed to load. Please try again later!</h3>}
         <Movies movies={this.state.movies}
                 showDetails={this.showDetails}
                 showAllMovies={this.showAllMovies}
