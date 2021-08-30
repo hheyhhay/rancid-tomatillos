16f6566c2c61633ab23f34a8e5a02d8d32b69a9d
Eric Li <ericli@knights.ucf.edu>
2021-08-30 16:19:13 -0600

    Revert to previous state

--- src/Details.js
+++ src/Details.js
@@ -4,19 +4,17 @@ import './images/back-arrow.svg';
 
 const Details=(props) => {
   return (
-    <div className='movie-details' >
-      <div className='movie-info'>
-        <h3 className='movie-title' > { props.title }</h3>
-        <p className='tagline' > { props.tagline }</p>
-        <p className='detail-rating' > ⭐️{ props.averageRating.toFixed(1) }</p>
-        <p className="movie-overview" > { props.overview }</p>
-        <p className='budget' > BUDGET: $ { props.budget }</p>
-        <p className='revenue' > REVENUE: $ { props.revenue }</p>
-        <p className='runtime' > RUNTIME: { props.runtime } mins </p>
-        <p className='movie-releaseDate' > Released on: { props.releaseDate }</p>
-        <img src={ '/back-arrow.svg' } className="arrow-icon" alt="Back arrow" onClick={ () => props.showAllMovies() }/>
-      </div>
-      <img className='movie-backdrop' src={ props.backdropPath } alt={`${props.title} backdrop image`}/>
+    <div className='movie-details'>
+      <img className='movie-backdrop' src={ props.backdropPath } alt={ `${props.title} backdrop image` }/>
+      <h3 className='movie-title'>{ props.title }</h3>
+      <p className='tagline'>{ props.tagline }</p>
+      <p className='detail-rating'>⭐️{ props.averageRating.toFixed(1) }</p>
+      <p className='movie-overview'>{ props.overview }</p>
+      <p className='budget'>BUDGET: $ { props.budget }</p>
+      <p className='revenue'>REVENUE: $ { props.revenue }</p>
+      <p className='runtime'>RUNTIME: { props.runtime } mins</p>
+      <p className='movie-releaseDate'>Released on: { props.releaseDate }</p>
+      <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => props.showAllMovies() }/>
     </div>
   )
 }
