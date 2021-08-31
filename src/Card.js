bd6b80b9f98eff869d79dd51b31da47f2a7d1593
Eric Li <ericli@knights.ucf.edu>
2021-08-30 23:05:53 -0600

    Remove extra elements from movie card

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,6 @@ const Card = (props) => {
   return (
     <div className='movie-card' onClick={ () => props.showDetails(props.id) } >
       <img className='movie-poster' src={ props.posterPath } alt={ `${props.title} poster image` }/>
-      <div className='movie-text-front'>
-        <h3 className='movie-title'>{ props.title }</h3>
-        <p className='movie-genres'>{ props.genres }</p>
-        <p className='movie-rating'>⭐️ { props.averageRating.toFixed(1) }</p>
-      </div>
     </div>
   )
 }
