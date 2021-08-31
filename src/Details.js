418c4440da8e93cfd43bc06e09e05ef58ae5e3d3
Eric Li <ericli@knights.ucf.edu>
2021-08-30 23:08:14 -0600

    Add conditional rendering to properties that may be 0 or empty

--- src/Details.js
+++ src/Details.js
@@ -6,13 +6,14 @@ const Details=(props) => {
   return (
     <div className='movie-details'>
       <div className='movie-info'>
-        
-        <p className='movie-title text'>{ props.title }</p>
-        <p className='tagline text'>{ props.tagline }</p>
+        <div className='title-container'>
+          <h1 className='movie-title text'>{ props.title }</h1>
+        </div>
+        { props.tagline && <p className='tagline text'><em>{ `"${props.tagline}"` }</em></p> }
         <p className='detail-rating text'>⭐️{ props.averageRating.toFixed(1) }</p>
         <p className='movie-overview text'>{ props.overview }</p>
-        <p className='budget text'>BUDGET: $ { props.budget }</p>
-        <p className='revenue text'>REVENUE: $ { props.revenue }</p>
+        { props.budget > 0 && <p className='budget text'>BUDGET: $ { props.budget }</p> }
+        { props.revenue > 0 && <p className='revenue text'>REVENUE: $ { props.revenue }</p> }
         <p className='runtime text'>RUNTIME: { props.runtime } mins</p>
         <p className='movie-releaseDate text'>Released on: { props.releaseDate }</p>
         <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => props.showAllMovies() }/>
