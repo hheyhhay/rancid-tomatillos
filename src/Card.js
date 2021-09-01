590c3827156aa338e26e674afd3b6c1c75736ca5
Hayley Dixon <hdixon37@gmail.com>
2021-09-01 14:44:56 -0600

    Combine Card into Movies

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './Card.css';
-import { Route, Link } from "react-router-dom";
-
-const Card = (props) => {
-  return (
-    <Link to={`/${props.id}`} className='movie-card'>
-       <img className='movie-poster' src={ props.posterPath } alt={ `${props.title} poster image` } onClick={ () => props.showDetails(props.id) }/>
-    </Link>
-  )
-}
-
-export default Card;
+// import React from 'react';
+// import './Card.css';
+// import { Route, Link } from "react-router-dom";
+//
+// const Card = (props) => {
+//   return (
+//     <Link to={`/${props.id}`} className='movie-card'>
+//        <img className='movie-poster' src={ props.posterPath } alt={ `${props.title} poster image` } onClick={ () => props.showDetails(props.id) }/>
+//     </Link>
+//   )
+// }
+//
+// export default Card;
 // Conditional Rendering to show indiviual cards
 //put onClick on the Card click
 //change to class component from function
 //
+
+
+// this.setState({ selectedMovie: data.movie })
