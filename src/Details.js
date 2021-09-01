364b0d2178f38a104f0ae26107718cac7470bb11
Hayley Dixon <hdixon37@gmail.com>
2021-09-01 15:26:07 -0600

    Fix showDetails function to be passed down properly

--- src/Details.js
+++ src/Details.js
@@ -4,10 +4,9 @@ import './images/back-arrow.svg';
 import { Route, Link } from "react-router-dom";
 
 
-const Details = ( { selectedMovie:movie }) => {
-  console.log('MOVIE', movie)
+const Details = ( { selectedMovie:movie, showAllMovies }) => {
+
  if (!movie.title) {
-   console.log('returns null')
    return null;
  } else {
     return (
@@ -16,7 +15,7 @@ const Details = ( { selectedMovie:movie }) => {
           <div className='title-container'>
             <h1 className='movie-title text'>{ movie.title }</h1>
           </div>
-          { movie.tagline && <p className='tagline text'><em>{ `"${movie.tagline}"` }</em></p> }
+          { movie.tagline && <p className='tagline text'><em>{ `"${ movie.tagline }"` }</em></p> }
           <p className='detail-rating text'>⭐️{ movie.average_rating.toFixed(1) }</p>
           <p className='movie-overview text'>{ movie.overview }</p>
           { movie.budget > 0 && <p className='budget text'>BUDGET: $ { movie.budget }</p> }
@@ -24,12 +23,11 @@ const Details = ( { selectedMovie:movie }) => {
           <p className='runtime text'>RUNTIME: { movie.runtime } mins</p>
           <p className='movie-releaseDate text'>Released on: { movie.release_date }</p>
           <Link to="/">
-            <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => movie.showAllMovies() }/>
+            <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => showAllMovies() }/>
           </Link>
         </div>
-        <img className='movie-cover' src={ movie.poster_path } alt={ `${movie.title}Poster` }/>
+        <img className='movie-cover' src={ movie.poster_path } alt={ `${ movie.title }Poster` }/>
         <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title }Backdrop` }/>
-
       </div>
     )
   }
