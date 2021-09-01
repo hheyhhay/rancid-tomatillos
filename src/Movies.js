96fc1a215714e42fdb3186fecef8f6df4ab52b5e
Hayley Dixon <hdixon37@gmail.com>
2021-09-01 14:45:11 -0600

    Combine Card into Movies

--- src/Movies.js
+++ src/Movies.js
@@ -2,53 +2,27 @@ import React from 'react';
 import Card from './Card';
 import Details from './Details';
 import './Movies.css';
+import './Card.css';
+
 import { Route, Link } from "react-router-dom";
 
 
-const Movies = ({ movies, showDetails, showAllMovies, selectedMovie }) => {
+const Movies = ({ movies, showDetails, showAllMovies }) => {
 
-  // const movieDetails =
-  //   return (
-  //     <Details
-  //       id={ selectedMovie['id'] }
-  //       posterPath={ selectedMovie['poster_path'] }
-  //       backdropPath={ selectedMovie['backdrop_path'] }
-  //       title={ selectedMovie['title'] }
-  //       averageRating={ selectedMovie['average_rating'] }
-  //       releaseDate={ selectedMovie'release_date'] }
-  //       overview={ selectedMovie['overview'] }
-  //       genres={ selectedMovie['genres'] }
-  //       budget={ selectedMovie['budget'] }
-  //       revenue={ selectedMovie['revenue'] }
-  //       runtime={ movie['runtime'] }
-  //       tagline={ movie['tagline'] }
-  //       showAllMovies={ showAllMovies }
-  //       />
-  //   )
 
   const movieCards = movies.map(movie => {
+    console.log(movie.posterPath)
+    console.log(movie)
+
     return (
-      <Card
-        id={ movie['id'] }
-        posterPath={ movie['poster_path'] }
-        backdropPath={ movie['backdrop_path'] }
-        title={ movie['title'] }
-        averageRating={ movie['average_rating'] }
-        releaseDate={ movie['release_date'] }
-        overview={ movie['overview'] }
-        genres={ movie['genres'] }
-        budget={ movie['budget'] }
-        revenue={ movie['revenue'] }
-        runtime={ movie['runtime'] }
-        tagline={ movie['tagline'] }
-        showDetails={ showDetails }
-        />
+      <Link to={`/${movie['id']}`} className='movie-card'>
+         <img className='movie-poster' src={ movie["poster_path"] } alt={ `${movie["title"]} poster image` } onClick={ () => movie.showDetails(movie["id"]) }/>
+      </Link>
     )
   })
   return (
     <div >
-      { movieCards.length === 1 && <div>{ selectedMovie }</div> }
-      { movieCards.length > 1 &&  <div className='movie-container'>{movieCards}</div> }
+      { movieCards.length &&  <div className='movie-container'>{movieCards}</div> }
     </div>
   )
 }
@@ -56,3 +30,21 @@ const Movies = ({ movies, showDetails, showAllMovies, selectedMovie }) => {
 export default Movies;
 
 // Tercinary the return!
+//  { movieCards.length === 1 && <div>{ selectedMovie }</div> }
+
+//
+// <Card
+//   id={ movie['id'] }
+//   posterPath={ movie['poster_path'] }
+//   backdropPath={ movie['backdrop_path'] }
+//   title={ movie['title'] }
+//   averageRating={ movie['average_rating'] }
+//   releaseDate={ movie['release_date'] }
+//   overview={ movie['overview'] }
+//   genres={ movie['genres'] }
+//   budget={ movie['budget'] }
+//   revenue={ movie['revenue'] }
+//   runtime={ movie['runtime'] }
+//   tagline={ movie['tagline'] }
+//   showDetails={ showDetails }
+//   />
