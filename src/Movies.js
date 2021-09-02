ebdf903b4f2106ae47e59168abe9ff7a78d08a3c
Eric Li <ericli@knights.ucf.edu>
2021-09-01 18:49:47 -0600

    Fix spacing and remove extra code

--- src/Movies.js
+++ src/Movies.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import './Movies.css';
-import './Card.css';
 
 import { Link } from "react-router-dom";
 
 const Movies = ( { movies } ) => {
-
   const movieCards = movies.map(movie => {
-
     return (
-      <Link to={ `/${movie['id']}` } className='movie-card'>
-         <img className='movie-poster' src={ movie["poster_path"] } alt={ `${movie["title"]} poster` } />
+      <Link to={ `/${movie['id']}` } id={ movie['id'] } className='movie-card'>
+         <img className='movie-poster' src={ movie["poster_path"] } alt={ `${movie["title"]} poster` }/>
       </Link>
     )
   })
 
   return (
     <div >
-      { movieCards.length &&  <div className='movie-container'>{movieCards}</div> }
+      { movieCards.length &&  <div className='movie-container'>{ movieCards }</div> }
     </div>
   )
 }
 
 export default Movies;
-
-// onClick={ () => showDetails(movie["id"]) }
