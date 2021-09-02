a807e66286860e94e72678943a0dc32ebce7ba5b
Eric Li <ericli@knights.ucf.edu>
2021-09-01 18:04:56 -0600

    Change Details from functional to class component

--- src/Details.js
+++ src/Details.js
@@ -1,36 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Details.css';
 import './images/back-arrow.svg';
 import { Link } from "react-router-dom";
+import { fetchSingleMovie } from './apiCalls';
 
+class Details extends Component {
+  constructor() {
+    super();
+      this.state = {
+        details: {}
+      }
+  }
 
-const Details = ( { selectedMovie:movie, showAllMovies }) => {
+  componentDidMount() {
+    fetchSingleMovie(this.props.selectedID)
+       .then(data => this.setState({ details: data.movie }))
+       .catch(error => this.setState({ error: error, isLoading: false }))
+  }
 
- if (!movie.title) {
-   return null;
- } else {
-    return (
-      <div className='movie-details'>
-        <div className='movie-info'>
-          <div className='title-container'>
-            <h1 className='movie-title text'>{ movie.title }</h1>
-          </div>
-          { movie.tagline && <p className='tagline text'><em>{ `"${ movie.tagline }"` }</em></p> }
-          <p className='detail-rating text'>⭐️{ movie.average_rating.toFixed(1) }</p>
-          <p className='movie-overview text'>{ movie.overview }</p>
-          { movie.budget > 0 && <p className='budget text'>BUDGET: $ { movie.budget }</p> }
-          { movie.revenue > 0 && <p className='revenue text'>REVENUE: $ { movie.revenue }</p> }
-          <p className='runtime text'>RUNTIME: { movie.runtime } mins</p>
-          <p className='movie-releaseDate text'>Released on: { movie.release_date }</p>
-          <Link to="/">
-            <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => showAllMovies() }/>
-          </Link>
-        </div>
-        <img className='movie-cover' src={ movie.poster_path } alt={ `${ movie.title } Poster` }/>
-        <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title } Backdrop` }/>
-      </div>
-    )
+  render() {
+    const { showAllMovies, selectedID } = this.props;
+    const movie = this.state.details;
+    if (!movie.title) {
+      return null;
+    } else {
+       return (
+         <div className='movie-details'>
+           <div className='movie-info'>
+             <div className='title-container'>
+               <h1 className='movie-title text'>{ movie.title }</h1>
+             </div>
+             { movie.tagline && <p className='tagline text'><em>{ `"${ movie.tagline }"` }</em></p> }
+             <p className='detail-rating text'>⭐️{ movie.average_rating.toFixed(1) }</p>
+             <p className='movie-overview text'>{ movie.overview }</p>
+             { movie.budget > 0 && <p className='budget text'>BUDGET: $ { movie.budget }</p> }
+             { movie.revenue > 0 && <p className='revenue text'>REVENUE: $ { movie.revenue }</p> }
+             <p className='runtime text'>RUNTIME: { movie.runtime } mins</p>
+             <p className='movie-releaseDate text'>Released on: { movie.release_date }</p>
+             <Link to="/">
+               <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' onClick={ () => showAllMovies() }/>
+             </Link>
+           </div>
+           <img className='movie-cover' src={ movie.poster_path } alt={ `${ movie.title } Poster` }/>
+           <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title } Backdrop` }/>
+         </div>
+       )
+     }
   }
 }
+
+
 
 export default Details;
