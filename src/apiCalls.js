4519c6633798f4845eb58ab3f22899b875090bb9
Hayley Dixon <hdixon37@gmail.com>
2021-09-03 08:57:13 -0600

    Change to async await

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,19 @@ export function fetchMovies() {
  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(response => response.json())
 }
 
-export function fetchSingleMovie(id) {
+export const fetchSingleMovie = async (id) => {
+  // try {
+    let url = `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`;
+    let response = await fetch(url);
+    if (!response.ok) {
+      console.log('response status', response.status)
+      throw new Error('We don\'t have that movie yet, sorry!')
+    }
+    let movieDetails = await response.json();
+    return movieDetails;
 
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(response => response.json())
+  // catch (e) {
+  //   console.log(e.message)
+  //   return e.message;
+  // }
 }
