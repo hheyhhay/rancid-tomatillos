6dddd09bb018e91da6880434afa0c3e368881e7b
Hayley Dixon <hdixon37@gmail.com>
2021-09-03 12:31:29 -0600

    Remove consoleLog and comments

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,14 +8,9 @@ export const fetchSingleMovie = async (id) => {
     let url = `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`;
     let response = await fetch(url);
     if (!response.ok) {
-      console.log('response status', response.status)
       throw new Error('We don\'t have that movie yet, sorry!')
     }
     let movieDetails = await response.json();
     return movieDetails;
 
-  // catch (e) {
-  //   console.log(e.message)
-  //   return e.message;
-  // }
 }
