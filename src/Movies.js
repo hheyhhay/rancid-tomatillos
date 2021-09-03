600c1ce09e33331e04b19978fc4b7f0f79abdb56
Hayley Dixon <hdixon37@gmail.com>
2021-09-03 12:18:55 -0600

    Assign Link a key for individual movies to fix console error message

--- src/Movies.js
+++ src/Movies.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Movies = ( { movies } ) => {
   const movieCards = movies.map(movie => {
     return (
-      <Link to={ `/${movie['id']}` } id={ movie['id'] } className='movie-card'>
+      <Link key={ movie['id'] } to={ `/${movie['id']}` } id={ movie['id'] } className='movie-card'>
          <img className='movie-poster' src={ movie["poster_path"] } alt={ `${movie["title"]} poster` }/>
       </Link>
     )
