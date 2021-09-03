e77b7daff5fcfe8c1212af4215e281920418ad0a
Hayley Dixon <hdixon37@gmail.com>
2021-09-03 12:18:32 -0600

    Change render return to hold conditionals and render an error

--- src/Details.js
+++ src/Details.js
@@ -21,51 +21,45 @@ class Details extends Component {
        .then(data => this.setState({ details: data.movie, isLoading: false }))
        .catch(error => this.setState({ error: error.message, isLoading: false }))
   }
-// cannot have conditional inside of a render...
+
   render() {
 
     const movie = this.state.details;
-    const { errorMessage }= this.state.error;
-    console.log(this.state.error)
-    // console.log(typeof this.state.error)
-    // console.log(this.state.error)
-    // console.log('move.title', movie.title)
-    debugger
-    { errorMessage === 'We don\'t have that movie yet, sorry!' && <p className="error">----------------------?- </p> }
-    console.log(movie.title)
-// Should we move the if (!movie.title) into a {react conditional}
-if (!movie.title) {
-      return null;
-    } else {
-       return (
-         <div className='movie-details'>
-           <div className='movie-info'>
-             <div className='title-container'>
-               <h1 className='movie-title text'>{ movie.title }</h1>
-             </div>
-             { movie.tagline && <p className='tagline text'><em>{ `"${ movie.tagline }"` }</em></p> }
-             <p className='detail-rating text'>{`⭐️  ${ movie.average_rating.toFixed(1)} `}</p>
-             <p className='movie-overview text'>{ movie.overview }</p>
-             { movie.budget > 0 && <p className='budget text'>Budget: $ { movie.budget }</p> }
-             { movie.revenue > 0 && <p className='revenue text'>Revenue: $ { movie.revenue }</p> }
-             <p className='runtime text'>Runtime: { movie.runtime } mins</p>
-             <p className='movie-releaseDate text'>Released on { dayjs(movie.release_date).format('MMMM DD, YYYY') }</p>
-             <Link to="/">
-               <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' />
-             </Link>
-           </div>
-           <img className='movie-cover' src={ movie.poster_path } alt={ `${ movie.title } Poster` }/>
-           <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title } Backdrop` }/>
-         </div>
-       )
-}
+
+    return (
+      <div >
+      {this.state.isLoading && <h2>Movie hasn't loaded yet</h2>}
+      {this.state.error && <div>
+        <h2> { this.state.error } </h2>
+        <Link to="/">
+          <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' />
+          </Link>
+          </div> }
+       {movie.title && !this.state.error &&
+        <div className='movie-details'>
+          <div className='movie-info'>
+            <div className='title-container'>
+              <h1 className='movie-title text'>{ movie.title }</h1>
+            </div>
+            { movie.tagline && <p className='tagline text'><em>{ `"${ movie.tagline }"` }</em></p> }
+            <p className='detail-rating text'>{`⭐️  ${ movie.average_rating.toFixed(1)} `}</p>
+            <p className='movie-overview text'>{ movie.overview }</p>
+            { movie.budget > 0 && <p className='budget text'>Budget: $ { movie.budget }</p> }
+            { movie.revenue > 0 && <p className='revenue text'>Revenue: $ { movie.revenue }</p> }
+            <p className='runtime text'>Runtime: { movie.runtime } mins</p>
+            <p className='movie-releaseDate text'>Released on { dayjs(movie.release_date).format('MMMM DD, YYYY') }</p>
+            <Link to="/">
+              <img src={ '/back-arrow.svg' } className='arrow-icon' alt='Back arrow' />
+            </Link>
+          <img className='movie-cover' src={ movie.poster_path } alt={ `${ movie.title } Poster` }/>
+          <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title } Backdrop` }/>
+        </div>
+      </div>
+    }
+    </div>
+    )
   }
-}
+ }
+
 
 export default Details;
-
-// if (errorMessage) {
-//   return (
-//     <h3 className='error'> errorMessage </h3>
-//   )
-// } else
