b5606acd4f1e35f1fcd8c73a6274de8e8920fb58
Hayley Dixon <hdixon37@gmail.com>
2021-09-03 08:58:10 -0600

    Update componentMount function to handle error

--- src/Details.js
+++ src/Details.js
@@ -9,19 +9,32 @@ class Details extends Component {
   constructor() {
     super();
       this.state = {
-        details: {}
+        details: {},
+        error: '',
+        isLoading: false
       }
   }
 
   componentDidMount() {
+    this.setState({ isLoading:true })
     fetchSingleMovie(this.props.selectedID)
-       .then(data => this.setState({ details: data.movie }))
-       .catch(error => this.setState({ error: error, isLoading: false }))
+       .then(data => this.setState({ details: data.movie, isLoading: false }))
+       .catch(error => this.setState({ error: error.message, isLoading: false }))
   }
-
+// cannot have conditional inside of a render...
   render() {
+
     const movie = this.state.details;
-    if (!movie.title) {
+    const { errorMessage }= this.state.error;
+    console.log(this.state.error)
+    // console.log(typeof this.state.error)
+    // console.log(this.state.error)
+    // console.log('move.title', movie.title)
+    debugger
+    { errorMessage === 'We don\'t have that movie yet, sorry!' && <p className="error">----------------------?- </p> }
+    console.log(movie.title)
+// Should we move the if (!movie.title) into a {react conditional}
+if (!movie.title) {
       return null;
     } else {
        return (
@@ -45,8 +58,14 @@ class Details extends Component {
            <img className='movie-backdrop' src={ movie.backdrop_path } alt={ `${ movie.title } Backdrop` }/>
          </div>
        )
-     }
+}
   }
 }
 
 export default Details;
+
+// if (errorMessage) {
+//   return (
+//     <h3 className='error'> errorMessage </h3>
+//   )
+// } else
