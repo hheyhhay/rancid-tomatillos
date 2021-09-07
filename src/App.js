f300f04296480a7b0a9fc000d40971c8861477c5
Hayley Dixon <hdixon37@gmail.com>
2021-09-06 18:50:12 -0600

    Add Search Compondent and search states

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Movies from './Movies.js';
 import { Route } from "react-router-dom";
 import Details from './Details';
+import Search from './Search';
 import { fetchMovies } from './apiCalls';
 
 class App extends Component {
@@ -11,7 +12,9 @@ class App extends Component {
       this.state = {
         movies: [],
         error: '',
-        isLoading: false
+        isLoading: false,
+        filteredMovies: [],
+        inputValue: ''
       }
   }
 
@@ -22,8 +25,16 @@ class App extends Component {
       .catch(error => this.setState({ error: error.message, isLoading: false }))
   }
 
+  stateChange = (key, newValue) => {
+    this.setState({ [key]: newValue})
+  }
+
   render() {
     const { movies, error, isLoading } = this.state;
+    console.log(this.filteredMovies)
+    // const filteredMovies = this.state.movies.filter(movie => {
+    //   return movie.title.includes(this.state.inputValue)
+    // })
     return (
       <main className='App'>
         <nav className='nav-bar'>
@@ -34,9 +45,16 @@ class App extends Component {
         </nav>
         { this.state.isLoading && <h3 className='error'>Loading Movies...</h3> }
         { this.state.error && <h3 className='error'>{ this.state.error }</h3> }
+        <Search id='search'
+          moviesSearch={ movies }
+          inputValue={this.state.inputValue}
+          stateChange = {this.stateChange}
+          />
+
         <Route exact path="/">
             <Movies id='movie'
               movies={ movies }
+              filteredMovies={ this.state.filteredMovies }
             />
         </Route>
         <Route exact path='/:id' render={ ({ match }) => {
